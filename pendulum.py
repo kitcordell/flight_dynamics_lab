3f8b6bec28335e3a449c4dc68406595fc47ecde4
kitcordell <72333001+kitcordell@users.noreply.github.com>
2026-02-19 22:18:31 -0800

    Add files via upload

--- pendulum.py
+++ pendulum.py
@@ -1,44 +1,45 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from integrators import euler
 from scipy.integrate import solve_ivp
 
-L = 1 # m
-g = 9.8 #m/s^2
-h = 0.01
+L = 1.0 # Pendulum Length, m
+g = 9.8 # Gravity, m/s^2
+h = 0.01  # step size
 
-theta0 = np.deg2rad(180) # initial angle, deg
-omega0 = np.deg2rad(0)
+theta0 = np.deg2rad(90) # Initial angle
+omega0 = np.deg2rad(0) # Initial Angular Velocity
+
+t0 = 0 # Simulation Start Time
+tf = 25.0 # Simulation end time
+
+# Pendulum State Space Form
+def pendulum(t, y, g, L):
+
+    dydt = np.zeros_like(y)
+    dydt[0] = y[1]
+    dydt[1] = -(g/L)*np.sin(y[0])
+    return dydt
+
+# Euler Integration
+t, y = euler(pendulum, (0.0, tf), [theta0, omega0], h, args=(g, L))
 
 
+# Python RK45 Integration
+sol = solve_ivp(pendulum,[0, tf],[theta0, omega0],args=(g, L),method="RK45",t_eval=t)
 
+plt.figure()
 
-simend = 10 # sim end time
-N = int(simend/h) # total number of data points 
-tspan = np.linspace(0,simend,N+1)  # 
+# Angular Position Plots
+plt.plot(t, np.rad2deg(y[:,0]), color="blue", label="θ Euler")
+plt.plot(sol.t, np.rad2deg(sol.y[0]), "--", color="blue", label="θ RK45")
 
-theta=np.zeros((N+1,2)) # initialize theta array
+# Angular Velocity Plots
+plt.plot(t, np.rad2deg(y[:,1]), color="red", label="ω Euler")
+plt.plot(sol.t, np.rad2deg(sol.y[1]), "--", color="red", label="ω RK45")
 
-theta[0][0] = theta0 # set initial position in matrix
-theta[0][1] = omega0 # set initial angular velocit in matrix
-
-def pendulum(theta,g,L):
-    omega = np.zeros(2) # initialize differential matrix
-
-    omega[0] = theta[1] # Pendulum ODE in state space
-    omega[1] = (-g/L) * np.sin(theta[0])
-
-    return omega 
-
-for i in range(0,N):
-    theta[i+1][:] = theta[i][:] + h * pendulum(theta[i][:],g,L) # Euler's method loop
-
-plt.figure(1)
-plt.plot(tspan, theta[:, 0]) # Angular Position Plot 
-plt.title("Angluar Position")
-
-plt.figure(2)
-plt.plot(tspan, theta[:,1]) # Angular Velocity Plot
-plt.title("Angular Velocity")
+plt.xlabel("Time (s)")
+plt.ylabel("State (deg / deg/s)")
+plt.title("Pendulum States")
+plt.legend()
 plt.show()
-
-
